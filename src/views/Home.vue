2ef1d38a859d65f3912bb95bd73b16ef5172314e
hjx2333 <hjx8085@163.com>
2020-08-13 14:37:30 +0800

    template

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="欢迎使用" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue"
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
